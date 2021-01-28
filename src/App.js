e2a0792c5dcdae74f18dffc640e73340eb4452b3
Saqib <saqib29111@gmail.com>
2021-01-28 23:52:37 +0600

    fetch API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
 
-
   const URL = `${process.env.REACT_APP_URL}&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_KEY}`;
-  console.log(URL);
+
+  const [counter, setCounter] = useState(0);
+  
+  useEffect(() => {
+    getRecipe();
+  }, []);
+
+  const getRecipe = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+  }
 
   return (
     <div className="App">
