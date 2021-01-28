6f392e530649e3dd71a9b377d8e6572abd6f987e
Saqib <saqib29111@gmail.com>
2021-01-28 22:23:43 +0600

    add search form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
@@ -9,7 +8,10 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
