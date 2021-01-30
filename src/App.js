33b3c39181eb892cf2e997d9fc95980ec0229446
Saqib <saqib29111@gmail.com>
2021-01-30 13:42:33 +0600

    show all recipes title, calories and image to DOM

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import Recipe from './Recipe';
 
 const App = () => {
 
   const URL = `${process.env.REACT_APP_URL}&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_KEY}`;
 
   const [counter, setCounter] = useState(0);
+
+  const [recipes, setRecipes] = useState([]);
   
   useEffect(() => {
-    getRecipe();
+    getRecipe(); 
   }, []);
 
   const getRecipe = async () => {
     const response = await fetch(URL);
     const data = await response.json();
-    console.log(data);
+    setRecipes(data.hits);
+    console.log(data.hits) 
   }
 
   return (
@@ -22,7 +26,10 @@ const App = () => {
       <form className="search-form">
         <input className="search-bar" type="text"/>
         <button className="search-button" type="submit">Search</button>
-      </form>
+      </form> 
+      {recipes.map(recipe => (
+        <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image}/>
+      ))}
     </div>
   );
 }
